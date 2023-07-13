7a1b9bc42168eba6bfaf69f05ce85b1ea3b8b01f
highjumpkickk <136947676+highjumpkickk@users.noreply.github.com>
2023-07-13 02:55:22 -0500

    Add Main.java

---
+++ decimal-comparator/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, -3.176));
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+
+        int firstCheck = (int) (num1 * 1000);
+        int secondCheck = (int) (num2 * 1000);
+
+        if (firstCheck == secondCheck) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
