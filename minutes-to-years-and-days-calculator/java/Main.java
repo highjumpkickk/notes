b3368f472ec2c3dd6044b64845abc9b22efc5c62
highjumpkickk <136947676+highjumpkickk@users.noreply.github.com>
2023-07-17 01:21:15 -0800

    Add Main.java

---
+++ minutes-to-years-and-days-calculator/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
+
+    public static void printYearsAndDays(long minutes) {
+
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long years = minutes / 525600;
+            long days = (minutes % 525600) / 1440;
+            String result = minutes + " min = " + years + " y and " + days + " d";
+            System.out.println(result);
+        }
+    }
+}
